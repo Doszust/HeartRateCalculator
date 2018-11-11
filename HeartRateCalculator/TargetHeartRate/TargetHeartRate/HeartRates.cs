f0848950c63ff645eeb3d0ccd5e73c5777322bc6
Dylan Oszust <Dyansh6594@yahoo.com>
2018-11-11 13:47:27 -0500

    Initial commit

---
+++ HeartRateCalculator/TargetHeartRate/TargetHeartRate/HeartRates.cs
@@ -0,0 +1,68 @@
+﻿// Dylan Oszust
+// May 27th, 2018 
+
+using System;
+
+class HeartRates
+{
+    private string fName; //First Name
+    private string lName; //Last Name
+    private int year = DateTime.Now.Year; //Current Year From System
+    private int yob; //Year Of Birth
+    private int age;
+    private int maxHR;
+    private string targetHR;
+
+    public void SetFName(string fName) //Sets Objects First Name
+    {
+        this.fName = fName;
+    }
+
+    public string GetFName() //Returns Objects First Name
+    {
+        return fName;
+    }
+
+    public void SetLName(string LName) //Sets Objects Last Name
+    {
+        this.lName = LName;
+    }
+
+    public string GetLName() //Returns Objects Last Name
+    {
+        return lName;
+    }
+
+    public void SetYOB(int yob) //Sets Objects Year Of Birth
+    {
+        this.yob = yob;
+    }
+
+    public int GetYOB() //Returns Objects Year Of Birth
+    {
+        return yob;
+    }
+
+    public int GetAge() //Calculates Users Age
+    {
+        age = year - yob;
+        CalcHeartRates(); //Calls Heart Rate Calculation Method At This Point
+        return age;
+    }
+
+    public int GetMaxHR() //Returns Max Heart Rate
+    {
+        return maxHR;
+    }
+
+    public string GetTargetHR() //Returns Target Heart Rate
+    {
+        return targetHR;
+    }
+
+    private void CalcHeartRates() //Calculates Max Heart Rate and Target Heart Rate Range
+    {
+        maxHR = 220 - age;
+        targetHR = $"{maxHR * .5} - {maxHR * .85}";
+    }
+}
---
+++ HeartRateCalculator/TargetHeartRate/TargetHeartRate/TargetHeartrates.cs
@@ -0,0 +1,39 @@
+﻿// Dylan Oszust
+// May 27th, 2018 
+
+using System;
+
+class TargetHeartRates
+{
+   static void Main(string[] args)
+   {
+        //instantiates new object
+        HeartRates myHeartRates = new HeartRates();
+
+        //user inputs first name
+        Console.Write("Enter First Name: ");
+        string fName = Console.ReadLine();
+
+        //user inputs last name
+        Console.Write("Enter Last Name: ");
+        string lName = Console.ReadLine();
+
+        //user inputs birth year
+        Console.Write("Enter Year Of Birth: ");
+        int YOB = int.Parse(Console.ReadLine());
+
+        //sets object attributes to user input values
+        myHeartRates.SetFName(fName);
+        myHeartRates.SetLName(lName);
+        myHeartRates.SetYOB(YOB);
+
+        //displays all information
+        Console.WriteLine($"First Name: {myHeartRates.GetFName()}\n" +
+            $"Last Name: {myHeartRates.GetLName()}\n" +
+            $"Year Of Birth: {myHeartRates.GetYOB()}\n" +
+            $"Age: {myHeartRates.GetAge()}\n" +
+            $"Max HR: {myHeartRates.GetMaxHR()} BPM\n" +
+            $"Target HR: {myHeartRates.GetTargetHR()} BPM\n");
+    }
+}
+
